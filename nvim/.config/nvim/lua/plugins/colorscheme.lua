5d1a5cd3440706e2d6fcfbeee34684463c1a5845
Tamás Mojzes <tmojzi96@gmail.com>
2024-01-02 20:44:25 +0100

    Added config files for my sway based gui env.

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,23 @@
+return {
+	{
+		"projekt0n/github-nvim-theme",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			require("github-theme").setup({
+				-- Put the settings here
+			})
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				-- Put the settings here
+			})
+		end,
+	},
+}
