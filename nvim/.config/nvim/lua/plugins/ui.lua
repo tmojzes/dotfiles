95dea26d9be6f74317e95cea8078712a982665f2
Tamás Mojzes <tamas.mojzes@nokia.com>
2024-01-03 14:48:03 +0100

    Set neotree to show dotfiles by default except .git and .DS_STORE

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -24,4 +24,24 @@ return {
 			draw = { animation = require("mini.indentscope").gen_animation.none() },
 		},
 	},
+
+	-- show hidden files in neotree
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		opts = {
+			filesystem = {
+				filtered_items = {
+					visible = false,
+					show_hidden_count = true,
+					hide_dotfiles = false,
+					hide_gitignored = false,
+					hide_by_name = {
+						".git",
+						".DS_Store",
+					},
+					never_show = {},
+				},
+			},
+		},
+	},
 }
