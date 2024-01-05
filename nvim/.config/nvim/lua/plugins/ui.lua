fff3fb6b478598e2d600b60a1eaceb80d641485e
Tamás Mojzes <tamas.mojzes@nokia.com>
2024-01-05 16:03:44 +0100

    Hide dotfiles by default

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -33,8 +33,8 @@ return {
 				filtered_items = {
 					visible = false,
 					show_hidden_count = true,
-					hide_dotfiles = false,
-					hide_gitignored = false,
+					hide_dotfiles = true,
+					hide_gitignored = true,
 					hide_by_name = {
 						".git",
 						".DS_Store",
