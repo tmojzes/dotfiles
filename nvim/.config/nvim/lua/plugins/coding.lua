e90daaafd7c2e0e4818030d54ed3feb4f581b5ca
Tamás Mojzes <tamas.mojzes@nokia.com>
2024-01-05 16:02:33 +0100

    Added none-ls plugin and configured go tools with it.

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -76,17 +76,28 @@ return {
 		keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
 		config = true,
 	},
-
-	-- linters
 	{
-		"mfussenegger/nvim-lint",
-		opts = {
-			linters_by_ft = {
-				-- global linter for spell checking
-				["*"] = { "codespell" },
-				-- linter for go
-				go = { "golangcilint" },
+		"nvimtools/none-ls.nvim",
+		optional = true,
+		dependencies = {
+			{
+				"williamboman/mason.nvim",
+				opts = function(_, opts)
+					opts.ensure_installed = opts.ensure_installed or {}
+					vim.list_extend(opts.ensure_installed, { "gomodifytags", "impl" })
+				end,
 			},
 		},
+		opts = function(_, opts)
+			local nls = require("null-ls")
+			opts.sources = vim.list_extend(opts.sources or {}, {
+				nls.builtins.code_actions.gomodifytags,
+				nls.builtins.code_actions.impl,
+				nls.builtins.formatting.goimports,
+				nls.builtins.formatting.gofumpt,
+				nls.builtins.diagnostics.golangci_lint,
+				nls.builtins.diagnostics.codespell,
+			})
+		end,
 	},
 }
