8035f6ceed58c9f2099b12612a7ca3e6558d95ca
Tamás Mojzes <tamas.mojzes@nokia.com>
2024-06-12 09:53:24 +0200

    feat(nvim): sets line length marker

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,6 +3,7 @@
 -- Add any additional options here
 vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
 vim.opt.tabstop = 4 -- the number of spaces inserted for a tab
+vim.opt.colorcolumn = "110" -- sets a vertical line length marker
 
 -- Add templ file extension to support go templ.
 vim.filetype.add({
