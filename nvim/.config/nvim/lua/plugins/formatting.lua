0a5347872633f1794253b52afff7b654e4e69dd5
Tamás Mojzes <tamas.mojzes@nokia.com>
2024-06-19 09:36:21 +0200

    feat(nvim): sets golines max-len to 110
    Golines will only format lines which are longer than 110.

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -2,6 +2,11 @@ return {
 	{
 		"stevearc/conform.nvim",
 		opts = {
+			formatters = {
+				golines = {
+					prepend_args = { "-m", "110" },
+				},
+			},
 			formatters_by_ft = {
 				python = { "isort", "black" },
 				lua = { "stylua" },
