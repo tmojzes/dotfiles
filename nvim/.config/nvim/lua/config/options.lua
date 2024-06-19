9f5f686f1f56f1cf974853e1c284b7b69ca4380b
Tamás Mojzes <tamas.mojzes@nokia.com>
2024-06-19 09:37:38 +0200

    feat(nvim): disables relative numbers

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -4,6 +4,8 @@
 vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
 vim.opt.tabstop = 4 -- the number of spaces inserted for a tab
 vim.opt.colorcolumn = "110" -- sets a vertical line length marker
+vim.opt.number = true
+vim.opt.relativenumber = false
 
 -- Add templ file extension to support go templ.
 vim.filetype.add({
