29eeca93bcdf04e396d3e2e837c9b0158e34d30f
Tamás Mojzes <tamas.mojzes@nokia.com>
2024-08-15 11:17:10 +0200

    chore(packages): renamed list and install packages to be system specific

--- scripts/.local/bin/list-arch.sh
+++ scripts/.local/bin/list-arch.sh
@@ -9,11 +9,11 @@ base_devel_packages=($(pacman -Sg base-devel | cut -f 2 -d " "))
 
 # Removes duplicates from other array
 for i in "${base_devel_packages[@]}"; do
-	all_packages=(${all_packages[@]//*$i*/})
+    all_packages=(${all_packages[@]//*$i*/})
 done
 
 all_packages=("${all_packages[@]/"$del"/}")
 
-printf "%s\n" "${all_packages[@]}" >${root_folder}/programs/.local/packages.list
+printf "%s\n" "${all_packages[@]}" >${root_folder}/programs/.local/arch_packages.list
 
-cat ${root_folder}/programs/.local/packages.list
+cat ${root_folder}/programs/.local/arch_packages.list
