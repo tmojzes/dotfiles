6885e54213629c0b3dda095a9fa8969a4c56f528
Tamás Mojzes <tamas.mojzes@nokia.com>
2024-08-15 11:18:21 +0200

    chore(scripts): run shell formatter

--- scripts/.local/bin/install-yay.sh
+++ scripts/.local/bin/install-yay.sh
@@ -5,9 +5,9 @@ script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 root_folder=$(cd ${script_dir} && git rev-parse --show-toplevel && cd ${current_folder})
 
 sudo pacman -S --needed git base-devel &&
-	git clone https://aur.archlinux.org/yay-bin.git ${root_folder}/yay-bin &&
-	cd ${root_folder}/yay-bin &&
-	makepkg -si &&
-	cd ${root_folder} &&
-	rm -rf yay-bin &&
-	cd ${current_folder}
+    git clone https://aur.archlinux.org/yay-bin.git ${root_folder}/yay-bin &&
+    cd ${root_folder}/yay-bin &&
+    makepkg -si &&
+    cd ${root_folder} &&
+    rm -rf yay-bin &&
+    cd ${current_folder}
