58c1b5e92631fc7bb813af9d6a772b09a9df8762
Tamás Mojzes <tmojzi96@gmail.com>
2025-06-19 17:05:10 +0200

    feat(scripts): add cachy os related scripts for package management

---
+++ scripts/.local/bin/list-cachy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+current_folder=${pwd}
+script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+root_folder=$(cd ${script_dir} && git rev-parse --show-toplevel && cd ${current_folder})
+
+all_packages=($(pacman -Qqe))
+base_devel_packages=($(pacman -Sg base-devel | cut -f 2 -d " "))
+
+# Removes duplicates from other array
+for i in "${base_devel_packages[@]}"; do
+    all_packages=(${all_packages[@]//*$i*/})
+done
+
+all_packages=("${all_packages[@]/"$del"/}")
+
+printf "%s\n" "${all_packages[@]}" >${root_folder}/programs/.local/cachy_packages.list
+
+cat ${root_folder}/programs/.local/cachy_packages.list
