e9f8462605dcbf7b9f3cd2524af57fa09de07baa
Tamás Mojzes <tamas.mojzes@ibm.com>
2025-07-09 10:38:52 +0200

    feat(bash): add rancher bin to PATH

--- bash/.bashrc.d/export.sh
+++ bash/.bashrc.d/export.sh
@@ -10,6 +10,6 @@ export WAYLAND_DISPLAY=wayland-0
 export GOBIN="$HOME/go/bin/"
 export ODIN_BIN="$HOME/projects/oss/Odin/"
 export BUN_BIN="/home/tmojzes/.bun/bin"
+export RANCHER_BIN="/Users/tmojzes/.rd/bin"
 
-export PATH=$PATH:$HOME/go/bin:$HOME/.local/bin:$HOME/.cargo/bin:$FLYCTL_INSTALL/bin:$ODIN_BIN:$BUN_BIN
-# export SOPS_AGE_KEY_FILE=$HOME/.local/share/sops/key
+export PATH=$PATH:$HOME/go/bin:$HOME/.local/bin:$HOME/.cargo/bin:$FLYCTL_INSTALL/bin:$ODIN_BIN:$BUN_BIN:$RANCHER_BIN
