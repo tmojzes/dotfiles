81fcc1fb6516a4794b2ff67e6e9da78b755e75ae
Tamás Mojzes <tamas.mojzes@ibm.com>
2025-07-09 10:50:09 +0200

    fix(zsh): use zsh compatible command check

--- zsh/.zshrc.d/completion.sh
+++ zsh/.zshrc.d/completion.sh
@@ -2,46 +2,46 @@
 autoload -U +X compinit && compinit
 
 # Completions
-if command -v talosctl &>/dev/null; then
+if (("$+commands[talosctl]")); then
     source <(talosctl completion zsh)
 fi
 
-if command -v kubectl &>/dev/null; then
+if (("$+commands[kubectl]")); then
     source <(kubectl completion zsh)
 fi
 
-if command -v operator-sdk &>/dev/null; then
+if (("$+commands[operator-sdk]")); then
     source <(operator-sdk completion zsh)
 fi
 
-if command -v kustomize &>/dev/null; then
+if (("$+commands[kustomize]")); then
     source <(kustomize completion zsh)
 fi
 
-if command -v k3d &>/dev/null; then
+if (("$+commands[k3d]")); then
     source <(k3d completion zsh)
 fi
 
-if command -v clusterctl &>/dev/null; then
+if (("$+commands[clusterctl]")); then
     source <(clusterctl completion zsh)
 fi
 
-if command -v helm &>/dev/null; then
+if (("$+commands[helm]")); then
     source <(helm completion zsh)
 fi
 
-if command -v gocomplete &>/dev/null; then
+if (("$+commands[gocomplete]")); then
     complete -C "$HOME/go/bin/gocomplete" go
 fi
 
-if command -v rustup &>/dev/null; then
+if (("$+commands[rustup]")); then
     source <(rustup completions zsh)
 fi
 
-if command -v tailscale &>/dev/null; then
+if (("$+commands[tailscale]")); then
     source <(tailscale completion zsh)
 fi
 
-if command -v ibmcloud &>/dev/null; then
+if (("$+commands[ibmcloud]")); then
     source /usr/local/ibmcloud/autocomplete/zsh_autocomplete
 fi
