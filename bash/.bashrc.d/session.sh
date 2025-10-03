9b6ef7bd582306c15bfea90b649e5af4cf64d5c1
Tamás Mojzes <tmojzi96@gmail.com>
2025-10-03 22:21:00 +0200

    chore(tmux): enable tmux when loading shell

--- bash/.bashrc.d/session.sh
+++ bash/.bashrc.d/session.sh
@@ -4,7 +4,7 @@ if command -v starship &>/dev/null; then
 fi
 
 # Starts tmux session.
-# if command -v tmux &>/dev/null; then
-#     # Start tmux session if shell is not running in one already.
-#     [[ -z "$TMUX" ]] && exec tmux
-# fi
+if command -v tmux &>/dev/null; then
+    # Start tmux session if shell is not running in one already.
+    [[ -z "$TMUX" ]] && exec tmux
+fi
