5c637d2533a7a5f6384097ee5d510d755a3d922b
Tamás Mojzes <tmojzi96@gmail.com>
2025-11-11 16:10:41 +0100

    feat(zsh): add direnv support

--- zsh/.zshrc.d/session.sh
+++ zsh/.zshrc.d/session.sh
@@ -8,3 +8,8 @@ if command -v tmux &>/dev/null; then
     # Start tmux session if shell is not running in one already.
     [[ -z "$TMUX" ]] && exec tmux
 fi
+
+# Direnv hook setup
+if command -v direnv &>/dev/null; then
+    eval "$(direnv hook zsh)"
+fi
