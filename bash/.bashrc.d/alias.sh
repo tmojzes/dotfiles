2fe7b021f46b7261f97ddf10efd0b32464f69423
Tamás Mojzes <tmojzi96@gmail.com>
2026-02-25 10:06:08 +0100

    feat(bash): add gemini alias for npx command

--- bash/.bashrc.d/alias.sh
+++ bash/.bashrc.d/alias.sh
@@ -4,6 +4,7 @@ alias ll='ls -lah --color=auto'
 alias grep='grep --color=auto'
 alias vim="nvim"
 alias ic="ibmcloud"
+alias gemini='npx @google/gemini-cli@preview'
 
 # alias sopse="sops --encrypt --age $(cat $SOPS_AGE_KEY_FILE | grep -oP 'public key: \K(.*)') -i"
 # alias sopsd="sops --decrypt --age $(cat $SOPS_AGE_KEY_FILE | grep -oP 'public key: \K(.*)') -i"
