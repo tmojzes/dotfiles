10ea22534fa1a1c23bfbebe1d211070efa97fe38
Tamás Mojzes <tmojzi96@gmail.com>
2026-03-20 17:46:55 +0100

    feat(nvim): use gofmt instead of golines with gofumpt

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -3,9 +3,9 @@ return {
 		"stevearc/conform.nvim",
 		opts = {
 			formatters = {
-				golines = {
-					prepend_args = { "--base-formatter", "gofumpt", "--max-len", "110" },
-				},
+				-- golines = {
+				-- 	prepend_args = { "--base-formatter", "gofumpt", "--max-len", "110" },
+				-- },
 				ruff_format = {
 					prepend_args = { "--line-length", "110" },
 				},
@@ -14,7 +14,7 @@ return {
 			formatters_by_ft = {
 				python = { "isort", "ruff_format" },
 				lua = { "stylua" },
-				go = { "golines", "goimports" },
+				go = { "gofmt" },
 				sh = { "shfmt" },
 				proto = { "buf" },
 				c = { "clang-format" },
