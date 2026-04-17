38ef4089ed5425d8fc3e6185fb82fae9c42d9a1a
Tamás Mojzes <tmojzi96@gmail.com>
2026-04-17 14:52:46 +0200

    feat(bash): use functions for ibmcloud aliases

--- bash/.bashrc.d/alias.sh
+++ bash/.bashrc.d/alias.sh
@@ -5,10 +5,48 @@ alias grep='grep --color=auto'
 alias vim="nvim"
 alias gemini='npx @google/gemini-cli@latest'
 
-# ibmcloud aliases
-alias ic='ibmcloud'
-alias ich='IBMCLOUD_HOME=~/ibmcloud_homes/hl_dev ibmcloud'
-alias icp='IBMCLOUD_HOME=~/ibmcloud_homes/personal ibmcloud'
+# Using functions instead of alias makes bash completion possible.
+if command -v ibmcloud &>/dev/null; then
+    ic() {
+        IBMCLOUD_HOME="" ibmcloud "$@"
+    }
+    complete -o default -F _bash_autocomplete ic
 
+    ich() {
+        IBMCLOUD_HOME="$HOME/ibmcloud_homes/hl_dev" ibmcloud "$@"
+    }
+    complete -o default -F _bash_autocomplete ich
+
+    icp() {
+        IBMCLOUD_HOME="$HOME/ibmcloud_homes/personal" ibmcloud "$@"
+    }
+    complete -o default -F _bash_autocomplete icp
+
+    icsm() {
+        IBMCLOUD_HOME="$HOME/ibmcloud_homes/sat_mesh" ibmcloud "$@"
+    }
+    complete -o default -F _bash_autocomplete icsm
+
+    icst() {
+        IBMCLOUD_HOME="$HOME/ibmcloud_homes/sat_test" ibmcloud "$@"
+    }
+    complete -o default -F _bash_autocomplete icst
+
+    icstl() {
+        IBMCLOUD_HOME="$HOME/ibmcloud_homes/sat_test_location" ibmcloud "$@"
+    }
+    complete -o default -F _bash_autocomplete icstl
+
+    icss() {
+        IBMCLOUD_HOME="$HOME/ibmcloud_homes/sat_stage" ibmcloud "$@"
+    }
+    complete -o default -F _bash_autocomplete icss
+
+    icad() {
+        IBMCLOUD_HOME="$HOME/ibmcloud_homes/argonauts_dev" ibmcloud "$@"
+    }
+    complete -o default -F _bash_autocomplete icad
+
+fi
 # alias sopse="sops --encrypt --age $(cat $SOPS_AGE_KEY_FILE | grep -oP 'public key: \K(.*)') -i"
 # alias sopsd="sops --decrypt --age $(cat $SOPS_AGE_KEY_FILE | grep -oP 'public key: \K(.*)') -i"
